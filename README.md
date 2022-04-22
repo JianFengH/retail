25dc679cedc21aeb65e1c41ee719ce817dc05371
JianFengH <707156268@qq.com>
2022-04-22 11:51:28 +0800

    Add mysql connect

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # retail
+
+## Initialize Database
+You can find sql files under the directory of `db` to initialize the database. You can use docker to run a MySQL server in your local enviornment.
+
+```
+docker run --name mysql -e MYSQL_ROOT_PASSWORD=yourpassword -d mysql
+```
+
+## Create a file `config.ini` under the root directory
+```
+[postgresql]
+host=
+database=
+user=
+password=
+``` 
+
+## How to test functions
+
+### connect
+```
+pip install -r requirements.txt
+
+python connect.py
+```
---
+++ connect.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+from mysql.connector import MySQLConnection, Error
+from config import config
+
+
+def connect():
+    """ Connect to MySQL database """
+
+    db_config = config()
+    conn = None
+    try:
+        print('Connecting to MySQL database...')
+        conn = MySQLConnection(**db_config)
+
+        if conn.is_connected():
+            print('Connection established.')
+        else:
+            print('Connection failed.')
+
+    except Error as error:
+        print(error)
+
+    finally:
+        if conn is not None and conn.is_connected():
+            conn.close()
+            print('Connection closed.')
+
+
+if __name__ == '__main__':
+    connect()
